658f31934de7b68b24d3b45e83d2cd1700f695d4
Gabriel Ramos <gcrepho@gmail.com>
2020-04-14 15:44:56 -0300

    added dashboard endpoint

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,7 @@
 class Album < ApplicationRecord
   belongs_to :category
   belongs_to :artist
+  
   has_many :songs
 
   validates :title, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
-Rails.application.routes.draw do
-  
+Rails.application.routes.draw do  
   devise_for :users
+  get 'home/index'
   root to: "home#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+    end
+  end
+
 end
