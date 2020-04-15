f6d170a4cabf419bd8049e04e1bfe3cf54103acf
Gabriel Ramos <gcrepho@gmail.com>
2020-04-15 14:38:10 -0300

    added react-bulma-components

---
+++ app/javascript/screens/favorites/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment, useState } from 'react';
+
+const FavoriteScreen = () => {
+  return(
+    <Fragment>
+      <div>Favorite Screen</div>
+    </Fragment>
+  );
+}
+
+export default FavoriteScreen;
