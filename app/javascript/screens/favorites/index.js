0cd1f261b2b2f9fdcebba825b0314d918ee41dbd
Gabriel Ramos <gcrepho@gmail.com>
2020-04-15 18:10:10 -0300

    added footer navbar

--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,9 +1,11 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
 
 const FavoriteScreen = () => {
   return(
     <Fragment>
       <div>Favorite Screen</div>
+      <NavbarFooter />
     </Fragment>
   );
 }
